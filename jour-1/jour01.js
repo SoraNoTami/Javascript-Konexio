641cd92b4b4f2daac6e8c3e27055e730a4651c27
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-01-25 14:58:52 +0100

    finish first day

---
+++ jour-1/jour01.js
@@ -0,0 +1,49 @@
+// 01 - Hello World
+
+console.log("Hello World !");
+
+// 02 - String
+
+var test = "My name is Ethan"
+
+console.log(test);
+
+// 03 - Concaténation
+
+var name = "Ethan";
+
+console.log("Nice to meet you " + name);
+
+// 04 - String Length
+
+var testLength = "I'm very long !";
+
+console.log(test.length);
+
+// 05 - Replace
+
+var food = "croissant is meh";
+food = food.replace("meh","so good");
+
+console.log(food);
+
+// 06 - Up and Down
+
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+
+console.log(basicUp);
+
+// 07 - Split
+
+var word = "banana";
+var letters = word.split("");
+
+console.log(letters);
+
+//08 - Template
+
+var age = "19";
+var template = "I'm " + age + " years old";
+
+console.log(template)
