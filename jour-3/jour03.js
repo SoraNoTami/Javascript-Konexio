e11c311e47dc651986148ae0346aaad092a0b3c7
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-01-27 17:08:57 +0100

    Finish Day 3 and 4

--- jour-3/jour03.js
+++ jour-3/jour03.js
@@ -12,10 +12,13 @@ console.log(ingredients[1]);
 let objects = ["pen","book","lamp"];
 objects.unshift("chair");
 console.log(objects);
+
 objects.pop();
 console.log(objects);
+
 objects.push("laptop");
 console.log(objects);
+
 objects.shift();
 console.log(objects);
 
@@ -24,6 +27,7 @@ console.log(objects);
 let numbers =[4,10,8,12,6];
 numbers.reverse();
 console.log(numbers);
+
 numbers.sort((a, b) => a - b);
 console.log(numbers);
 
